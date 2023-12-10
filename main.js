557911cf1e21390a10b027811eca20e5ac1f00b4
aaravss <104297694+aaravss@users.noreply.github.com>
2023-12-10 16:07:31 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+
 function preload(){
     song1 = loadSound("i-wanna-feel-110039.mp3");
      song2 = loadSound("twisted-138700.mp3");
@@ -8,10 +13,28 @@ function setup(){
     canvas.position(450,120);
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw(){
     translate(620,0);
     scale(-1,1);
     image(video,0,0,620,500);
+}
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log(rightWristX);
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log(rightWristY);
+        console.log(leftWristX);
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log(leftWristY);
+    }
 }
