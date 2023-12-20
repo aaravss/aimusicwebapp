30b4227f214fa990d0f6d4c362ce0997d1cf7653
aaravss <104297694+aaravss@users.noreply.github.com>
2023-12-20 16:41:22 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-rightWristX = 0;
-rightWristY = 0;
-leftWristX = 0;
-leftWristY = 0;
+rightearX = 0;
+rightearY = 0;
+leftearX = 0;
+leftearY = 0;
+leftearScore = 0;
+song1status = "";
+song2status = "";
+
 
 function preload(){
     song1 = loadSound("i-wanna-feel-110039.mp3");
@@ -21,6 +25,17 @@ function draw(){
     translate(620,0);
     scale(-1,1);
     image(video,0,0,620,500);
+    fill("#FF00E1");
+    stroke("#FF00E1");
+    song1status = song1.isPlaying();
+    if(leftearScore>0.8){
+        circle(leftearX, leftearY, 20);
+        song2.stop();
+        if(song1status == false){
+            song1.play();
+            document.getElementById("song").innerHTML = "Song: I Wanna Feel";
+        }
+    }
 }
 
 function modelLoaded(){
@@ -30,11 +45,14 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log(rightWristX);
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log(rightWristY);
-        console.log(leftWristX);
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log(leftWristY);
+        console.log(rightearX);
+        rightearY = results[0].pose.rightEar.y;
+        rightearX = results[0].pose.rightEar.x;
+        console.log(rightearY);
+        console.log(leftearX);
+        leftearY = results[0].pose.leftEar.y;
+        leftearX = results[0].pose.leftEar.x;
+        console.log(leftearY);
+        leftearScore = results[0].pose.keypoints[3].score;
     }
 }
